b4d7e0f5e6b73a11439fbfa5d3965fb5dd198c23
Narsireddy-1118 <narsireddy225b@gmail.com>
2025-01-30 14:08:15 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 80; // Port 80 is the default HTTP port
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server listening at http://localhost:${port}`);
 });
